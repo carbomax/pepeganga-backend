a1ac134e5a4c3613010e05ecb9a2285af00bae94
luissangge <luis.carbonel1991@gmail.com>
2020-09-03 23:03:02 -0300

    firt commit

---
+++ pepe-ganga/eureka-service/src/test/java/uy/com/pepeganga/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.pepeganga.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
