3059415d4368e658a27222d1fd44828b0868ff70
luissangge <luis.carbonel1991@gmail.com>
2020-09-04 22:52:19 -0300

    Update dependencies user-service

--- pepe-ganga/user-service/src/main/java/uy/com/pepeganga/userservice/UserServiceApplication.java
+++ pepe-ganga/user-service/src/main/java/uy/com/pepeganga/userservice/UserServiceApplication.java
@@ -2,7 +2,9 @@ package uy.com.pepeganga.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
 
