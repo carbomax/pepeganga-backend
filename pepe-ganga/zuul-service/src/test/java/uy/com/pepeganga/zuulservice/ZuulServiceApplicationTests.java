f483edffef98cc39a6f8b87b9366d209fd815b20
luissangge <luis.carbonel1991@gmail.com>
2020-09-05 13:52:32 -0300

    Create microservice: zuul-service Modify user-service to communicate with zuul-service

---
+++ pepe-ganga/zuul-service/src/test/java/uy/com/pepeganga/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.pepeganga.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
