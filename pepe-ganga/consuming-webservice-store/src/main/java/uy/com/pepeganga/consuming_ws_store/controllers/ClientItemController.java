049884524a7c318ca2bceca69a0c071ebe7d2818
DESKTOP-BKAM140\Yariel Silva <yariel.usher@gmail.com>
2020-09-13 19:06:50 -0300

    some changes in the consuming-webservice-store: 1- The database temporally was implemented. 2- some models class were created to transform the original.

--- pepe-ganga/consuming-webservice-store/src/main/java/uy/com/pepeganga/consuming_ws_store/controllers/ClientItemController.java
+++ pepe-ganga/consuming-webservice-store/src/main/java/uy/com/pepeganga/consuming_ws_store/controllers/ClientItemController.java
@@ -1,12 +1,14 @@
 package uy.com.pepeganga.consuming_ws_store.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import uy.com.pepeganga.consuming_ws_store.models.ItemModelResponse;
 import uy.com.pepeganga.consuming_ws_store.services.ItemService;
-import uy.com.pepeganga.consuming_ws_store.wsdl.items.CargaArticulosExecuteResponse;
 
 
 @RestController
@@ -17,7 +19,12 @@ public class ClientItemController {
 	ItemService items;
 	
 	@GetMapping("/items")
-	public CargaArticulosExecuteResponse getItems() {
+	public List<ItemModelResponse> getItems() {
 		return items.getItems();
+	}
+	
+	@GetMapping("/itemsTemporally")
+	public List<ItemModelResponse> getItemsTemporally() {
+		return items.getItemsTemporally();
 	}
 }
