934fa7ca3ac1fd484bb486728e365e8148c28526
luissangge <luis.carbonel1991@gmail.com>
2020-09-19 13:35:14 -0300

    update

--- pepe-ganga/products-service/src/main/java/uy/com/pepeganga/productsservice/controllers/ItemController.java
+++ pepe-ganga/products-service/src/main/java/uy/com/pepeganga/productsservice/controllers/ItemController.java
@@ -3,9 +3,17 @@ package uy.com.pepeganga.productsservice.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import uy.com.pepeganga.productsservice.entities.Item;
 import uy.com.pepeganga.productsservice.gridmodels.ItemGrid;
 import uy.com.pepeganga.productsservice.services.ItemService;
 
@@ -14,13 +22,17 @@ import uy.com.pepeganga.productsservice.services.ItemService;
 public class ItemController {
 
 	@Autowired
-	ItemService item;
+	ItemService itemService;
 
 	@RequestMapping("/itemslist")
 	public List<ItemGrid> getItemsListGrid(){
-		return item.getItemsListGrid();
+		return itemService.getItemsListGrid();
 	}
 	
-	
+	@GetMapping("/items-by-filters/{page}/{size}")
+	public ResponseEntity< Page<Item>> getItemsByFilters(@RequestParam String sku, @RequestParam String nameProduct, 
+			@RequestParam String categoryNameDescription, @RequestParam double minPrice,@RequestParam double maxPrice, @PathVariable int page, @PathVariable int size){
+		return new ResponseEntity<>(itemService.findAll(sku.trim(), nameProduct.trim(), categoryNameDescription.trim(), minPrice, maxPrice, PageRequest.of(page, size)), HttpStatus.OK);
+	}
 	
 }
--- pepe-ganga/products-service/src/main/java/uy/com/pepeganga/productsservice/services/ItemService.java
+++ pepe-ganga/products-service/src/main/java/uy/com/pepeganga/productsservice/services/ItemService.java
@@ -1,12 +1,21 @@
 package uy.com.pepeganga.productsservice.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import uy.com.pepeganga.productsservice.clients.IStoreServiceClient;
+import uy.com.pepeganga.productsservice.entities.Item;
 import uy.com.pepeganga.productsservice.gridmodels.ItemGrid;
+import uy.com.pepeganga.productsservice.repository.ProductsRepository;
 
 @Service
 public class ItemService {
@@ -14,8 +23,29 @@ public class ItemService {
 	@Autowired
 	IStoreServiceClient client;
 	
+	@Autowired
+	ProductsRepository productsRepository;
+	
 	public List<ItemGrid> getItemsListGrid()
 	{
 		return client.getItemsGridTemporally();
+	}
+	
+	public Page<Item> findAll(String sku, String nameProduct, String categoryNameDescription, double minPrice, double maxPrice, Pageable pageable) {
+		return  productsRepository.findAll((Root<Item> root, CriteriaQuery<?> query, CriteriaBuilder cb) -> {
+					List<Predicate> predicates = new ArrayList<>();
+					predicates.add(cb.between(root.get("precioPesos"), minPrice, maxPrice));
+					if(!sku.equals("")) {
+						predicates.add(cb.like(root.get("sku").as(String.class), "%"+ sku + "%"));
+					}
+					if(!categoryNameDescription.equals("")) {
+						predicates.add(cb.like(root.get("artDescripCatalogo").as(String.class), "%"+ nameProduct + "%"));
+					}
+					if(!categoryNameDescription.equals("")){
+						predicates.add(cb.like(root.join("categories").get("description").as(String.class),"%"+ categoryNameDescription + "%"));
+					}
+					return query.where(predicates.toArray(new Predicate[predicates.size()])).getRestriction();
+				}, pageable);
+ 
 	}
 }
