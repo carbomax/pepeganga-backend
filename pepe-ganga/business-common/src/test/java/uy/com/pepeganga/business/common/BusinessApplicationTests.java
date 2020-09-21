fb3c5fae37fb6ce2c81268f9ab1608f5606130a6
luissangge <luis.carbonel1991@gmail.com>
2020-09-21 10:42:48 -0300

    Update Business commons

---
+++ pepe-ganga/business-common/src/test/java/uy/com/pepeganga/business/common/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.pepeganga.business.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
