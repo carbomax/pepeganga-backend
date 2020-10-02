f8116b0ddbf6e75f17d07f7acf5c583d4962ca07
luissangge <luis.carbonel1991@gmail.com>
2020-10-01 21:50:04 -0300

    Crud users. Add caching service to

--- pepe-ganga/business-common/src/main/java/uy/com/pepeganga/business/common/utils/conversions/ConversionClass.java
+++ pepe-ganga/business-common/src/main/java/uy/com/pepeganga/business/common/utils/conversions/ConversionClass.java
@@ -9,15 +9,18 @@ public class ConversionClass {
 
 	public static List<Image>separateImages(byte[] image){
 		if(image == null)
-			return new ArrayList<Image>();
+			return new ArrayList<>();
 		
-		List<Image> images = new ArrayList<Image>();
+		List<Image> images = new ArrayList<>();
 		String imageS = new String(image);		
 		
 		String[] imageList = imageS.split("%");
+		// aqui debes inicializar el objeto dentro del for
 		Image ima = new Image();
 		
 		for (String item : imageList) {
+
+			// El $ es un carcateres especial para las expresiones regulares
 			String[] parts = item.split("$");
 			ima.setId(Integer.decode(parts[0]));
 			ima.setPhotos(parts[1]);
