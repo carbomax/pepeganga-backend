34ec07086e65e359097bd7d2d853ad886ccb8fce
DESKTOP-BKAM140\Yariel Silva <yariel.usher@gmail.com>
2020-10-12 20:00:41 -0300

    config

---
+++ pepe-ganga/upload-file-service/src/main/java/uy/com/pepeganga/uploadfileservice/UploadFileServiceApplication.java
@@ -0,0 +1,13 @@
+package uy.com.pepeganga.uploadfileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileServiceApplication.class, args);
+	}
+
+}
---
+++ pepe-ganga/upload-file-service/src/test/java/uy/com/pepeganga/uploadfileservice/UploadFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.pepeganga.uploadfileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
