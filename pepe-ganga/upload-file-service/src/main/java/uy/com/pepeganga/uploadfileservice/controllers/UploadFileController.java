bcfc40aff8dfa465d4cbf303d6c457a3ac2dd306
DESKTOP-BKAM140\Yariel Silva <yariel.usher@gmail.com>
2020-10-15 13:11:25 -0300

    delete, update and added image done

--- pepe-ganga/upload-file-service/src/main/java/uy/com/pepeganga/uploadfileservice/controllers/UploadFileController.java
+++ pepe-ganga/upload-file-service/src/main/java/uy/com/pepeganga/uploadfileservice/controllers/UploadFileController.java
@@ -2,15 +2,18 @@ package uy.com.pepeganga.uploadfileservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import uy.com.pepeganga.business.common.models.ReasonResponse;
 import uy.com.pepeganga.uploadfileservice.services.FileUploadService;
+
+import javax.activation.FileTypeMap;
+import java.io.File;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -20,23 +23,44 @@ public class UploadFileController {
 	FileUploadService fileService;
 
 	@PostMapping("/file/upload-file")
-	public ResponseEntity<String> uploadFile(@RequestBody MultipartFile image){
+	public ResponseEntity<ReasonResponse> uploadFile(@RequestBody MultipartFile image){
 		if(image == null || image.isEmpty()){
 			throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, String.format("Archivo no válido"));
 		}		
-		try {			
-			return new ResponseEntity<String>(fileService.uploadFile(image), HttpStatus.OK);
+		try {
+			return new ResponseEntity<ReasonResponse>(fileService.uploadFile(image), HttpStatus.OK);
 		}
 		catch (Exception e) {
 			// TODO: handle exception
 			throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("Error almacenando archivo %s", e.getMessage()));
 		}		
 	}
-	
-	@PostMapping("/file/upload")
-	public ResponseEntity<String> upload(@RequestBody String image){
-		return new ResponseEntity<>(new String(), HttpStatus.OK);
-		
+
+	@GetMapping("/file/{name}")
+	public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) {
+		try {
+			byte[] image = fileService.getImage(name);
+
+			StringBuilder builder = fileService.buildURI(false);
+			builder.append(name);
+			File img = new File(builder.toString());
+
+			return ResponseEntity.ok().contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(img))).body(image);
+		}
+		catch (Exception e){
+			throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("Error obteniendo imagen del servidor", e.getMessage()));
+		}
 	}
-	
+
+	@DeleteMapping("/file-delete/{nameList}")
+	public ResponseEntity<?> deleteImage(@PathVariable("nameList") List<String> nameList) {
+		try {
+			fileService.deleteImages(nameList);
+			return new ResponseEntity(HttpStatus.OK);
+		}
+		catch (Exception e){
+			throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("Error obteniendo imagen del servidor", e.getMessage()));
+		}
+	}
+
 }
