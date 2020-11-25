697921a59f6dc76167616acde587397c440038e6
luissangge <luis.carbonel1991@gmail.com>
2020-11-24 22:07:24 -0300

    updating orders and operations - version 1.1

--- pepe-ganga/meli-service/src/main/java/uy/pepeganga/meli/service/utils/MeliUtils.java
+++ pepe-ganga/meli-service/src/main/java/uy/pepeganga/meli/service/utils/MeliUtils.java
@@ -1,24 +1,54 @@
 package uy.pepeganga.meli.service.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uy.com.pepeganga.business.common.entities.SellerAccount;
 import uy.com.pepeganga.business.common.utils.date.DateTimeUtilsBss;
 
 @Component
 public class MeliUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(MeliUtils.class);
 
     @Value("${meli.api.expiration.token}")
     private String tokenExpiration;
 
     private static int TOKEN_EXPIRATION;
 
-    public static boolean validateTokenExpiration(long timeTokenExpirationInMilleSeconds){
-        return DateTimeUtilsBss.getDurationOfMilleSeconds(DateTimeUtilsBss.getDateTimeAtCurrentTime().getMillis(), timeTokenExpirationInMilleSeconds ).getStandardMinutes() >= TOKEN_EXPIRATION;
-    }
-
     @Value("${meli.api.expiration.token}")
     public void setNameStatic(String tokenExpiration){
         MeliUtils.TOKEN_EXPIRATION = Integer.parseInt(this.tokenExpiration);
     }
+
+    /**
+     * Does some thing in old style.
+     * This method will be removed
+     * @deprecated use {@link #isExpiredToken(SellerAccount sellerAccount)} instead. Example if(MeliUtils.isExpiredToken(object))
+     */
+    @Deprecated(since = "1.1")
+    public static boolean validateTokenExpiration(long timeTokenExpirationInMilleSeconds){
+        return DateTimeUtilsBss.getDurationOfMilleSeconds(DateTimeUtilsBss.getDateTimeAtCurrentTime().getMillis(), timeTokenExpirationInMilleSeconds ).getStandardMinutes() >= TOKEN_EXPIRATION;
+    }
+
+    public static boolean isExpiredToken(SellerAccount sellerAccount){
+
+      try {
+          long currentTime = DateTimeUtilsBss.getLongDateTimeAtCurrentTime();
+          if(sellerAccount.getDateLastSynchronization() > currentTime || sellerAccount.getDateLastSynchronization() < currentTime){
+              return true;
+          } else {
+              return DateTimeUtilsBss.getDurationOfMilleSeconds(DateTimeUtilsBss.getDateTimeAtCurrentTime().getMillis(), sellerAccount.getExpirationDate() ).getStandardMinutes() <= TOKEN_EXPIRATION;
+          }
+      } catch (Exception e){
+          logger.error(e.getMessage());
+          return true;
+      }
+
+
+    }
+
+
 
 }
